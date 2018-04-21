fecf864440a7abbf8bd806a6bfcbacd1dc01e26a
Artem <caut19@gmail.com>
2018-04-21 15:34:39 +0200

    added post scaffold

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,47 @@
 class HomeController < ApplicationController
   def index
-    @test = "JOPA PASHI"
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_attributes)
+
+    if @post.save!
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_attributes)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def post_attributes
+    params.require(:post).permit(:image, :title, :body)
   end
 end
