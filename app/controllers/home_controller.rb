ca121a1b88e7676301096368d3d85cabcb1325b6
Artem <caut19@gmail.com>
2018-06-10 20:37:44 +0200

    feature/styled auth forms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,46 +4,4 @@ class HomeController < ApplicationController
   def index
     @posts = Post.all
   end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_attributes.merge(user_id: current_user.id))
-
-    if @post.save!
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-
-    if @post.update(post_attributes)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @post = Post.find(params[:id])
-
-    if @post.destroy
-      redirect_to root_path
-    end
-  end
-
-  private
-
-  def post_attributes
-    params.require(:post).permit(:image, :title, :body)
-  end
 end
